f18cdfc42b9515b6868e543762c51bf51cc335d5
Héctor Carramiñana <hector.crm.hcca@gmail.com>
2021-04-16 10:02:07 +0200

    detalles asociados al commit

--- client/src/service/user.service.js
+++ client/src/service/user.service.js
@@ -4,7 +4,7 @@ export default class UserService {
 
     constructor() {
         this.apiHandler = axios.create({
-            baseURL: `http://localhost:5000/api/users`,
+            baseURL: `${process.env.REACT_APP_API_URL}/api/users`,
             withCredentials: true
         })
     }
