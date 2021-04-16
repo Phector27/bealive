3619d19f43d8b9d201482cbd64d69187c0ecd3ff
Héctor Carramiñana <hector.crm.hcca@gmail.com>
2021-04-16 11:59:12 +0200

    changes CORS

--- server/configs/cors.config.js
+++ server/configs/cors.config.js
@@ -1,13 +1,12 @@
-const cors = require("cors");
+const cors = require('cors')
 
 const whitelist = [process.env.DOMAIN_REMOTE, process.env.DOMAIN_LOCAL]
 
 const corsOptions = {
-  origin: (origin, cb) => {
-    const originIsWhitelisted = whitelist.includes(origin);
-    cb(null, originIsWhitelisted);
-  },
-  credentials: true,
-};
+    origin: (origin, cb) => {
+        const originIsWhitelisted = whitelist.includes(origin)
+        cb(null, originIsWhitelisted)
+    }, credentials: true
+}
 
-module.exports = (app) => app.use(cors(corsOptions));
+module.exports = app => app.use(cors(corsOptions))
